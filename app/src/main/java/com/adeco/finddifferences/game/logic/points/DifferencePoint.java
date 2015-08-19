b73be49f12a50d3af10f7c389e3521465cea6372
Alex Gorbach <aleks.gorbach@gmail.com>
2015-08-19 09:07:03 +0300

    Restart difference handling fix

--- app/src/main/java/com/adeco/finddifferences/game/logic/points/DifferencePoint.java
+++ app/src/main/java/com/adeco/finddifferences/game/logic/points/DifferencePoint.java
@@ -6,25 +6,22 @@ public class DifferencePoint {
     private int y;
     private int radius;
 
-    public DifferencePoint(int x, int y, int radius) {
-        this.x = x;
-        this.y = y;
-        this.radius = radius;
+    public DifferencePoint(double x, double y, double radius) {
+        this.x = (int) x;
+        this.y = (int) y;
+        this.radius = (int) radius;
     }
 
     public boolean Check(int x, int y) {
         return GetDistance(x, y, this.x, this.y) <= radius;
     }
 
-    private static double GetDistance(int x, int y, int x1, int y1) {
+    private static double GetDistance(double x, double y, double x1, double y1) {
         return Math.sqrt((x - x1) * (x - x1) + (y - y1) * (y - y1));
     }
 
     public DifferencePoint scale(double scaleFactor) {
-        x *= scaleFactor;
-        y *= scaleFactor;
-        radius *= scaleFactor;
-        return this;
+        return new DifferencePoint(x * scaleFactor, y * scaleFactor, radius * scaleFactor);
     }
 
     public int getX() {
